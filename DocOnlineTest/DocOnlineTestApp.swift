e05c08f797ef757471ebeab507cb66307acc28af
StasiaKuzya <kuzina-sta@mail.ru>
2024-04-09 17:12:37 +0300

    Initial Commit

---
+++ DocOnlineTest/DocOnlineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocOnlineTestApp.swift
+//  DocOnlineTest
+//
+//  Created by Анастасия on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocOnlineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
